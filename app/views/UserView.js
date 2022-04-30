f09b5f40bf939dff7f3e87fb684cb0c151f01096
Marco <macdev.gmail@gmail.com>
2022-04-30 18:56:31 -0500

    added a test to return error when missing properties in payload and failed on purpose

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -5,6 +5,8 @@ class UserView {
     static createUser (payload){
         if (payload === null){
             return {error: "payload no existe"}
+        }else if (typeof payload.username != 'string' && typeof payload.name != 'string'  && typeof payload.id != 'int'){ 
+            return   {error: "necesitan tener un valor válido"}
         } else {
             return UserService.create(payload.id, payload.username, payload.name)
         }         
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -6,8 +6,13 @@ describe("Tests for UserView", ()=>{
     const result = UserView.createUser(payload)
     expect(result.error).toMatch(/payload no existe/)
   });
-  test('2. Return an error when try to create a new user with payload wih invalid properties', ()=>{
+  test('2. Return an error when try to create a new user with payload with invalid properties', ()=>{
     const payload = {username: null, name: 12, id: "id" }
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  });
+  test('3. Return error object when try to create a new user with a payload with missing properties', ()=>{
+    const payload = {username: "Username"}
     const result = UserView.createUser(payload)
     expect(result.error).toMatch(/necesitan tener un valor válido/)
   });
