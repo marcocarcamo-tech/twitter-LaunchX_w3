95f4c4116443389b9213069d3107242784d67ffe
Marco <macdev.gmail@gmail.com>
2022-04-30 02:37:54 -0500

    added a test for get user info in list and failed on purpose

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,4 +9,12 @@ describe("Test for UserService", ()=>{
     expect(user.id).toBe(1)
     expect(user.bio).not.toBeUndefined()
   });
+   test('2. Get all user data in a list', ()=>{
+    const user = UserService.create(1, "marcocar", "Marco")
+    const userInfoInList = UserService.getInfo(user)
+    expect(userInfoInList[0]).toBe(1)
+    expect(userInfoInList[1]).toBe("marcocar")
+    expect(userInfoInList[2]).toBe("Marco")
+    expect(userInfoInList[3]).not.toBeUndefined("No Bio")
+  });
 })
