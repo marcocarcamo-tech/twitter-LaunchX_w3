a3ae76206f7dc5847bb004f6c31de26e810d78ca
Marco <macdev.gmail@gmail.com>
2022-04-30 00:32:31 -0500

    test modified datecreated and last uptdated not undefined and passed orrectly

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/user')
 
 describe("Unit test for User class", ()=>{
   test('Create an User object', ()=>{
-    const user = new User(1, "marcocar", "Marco", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1, "marcocar", "Marco", "Bio")
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("marcocar")
     expect(user.name).toBe("Marco")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
   });
 })
