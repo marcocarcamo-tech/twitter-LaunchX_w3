495e79b60feef02c45b542dde866d216b3a0f384
Marco <macdev.gmail@gmail.com>
2022-04-30 05:14:25 -0500

    test for validating payload data added but failed on purpose

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -6,4 +6,9 @@ describe("Tests for UserView", ()=>{
     const result = UserView.createUser(payload)
     expect(result.error).toMatch(/payload no existe/)
   });
+  test('2. Return an error when try to create a new user with payload wih invalid properties', ()=>{
+    const payload = {username: null, name: 12, id: "id" }
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  });
 })
