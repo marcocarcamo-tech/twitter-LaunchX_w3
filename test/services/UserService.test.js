1f07d18228e0ef352ce75a6d77db7976dc531ff1
Marco <macdev.gmail@gmail.com>
2022-04-30 02:28:12 -0500

    create user with service test passed with functional code

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,11 +1,12 @@
-const UserService = require(../../app/services/UserService);
+const UserService = require('../../app/services/UserService');
 
 describe("Test for UserService", ()=>{
   test('1. Create a new user using the UserService', ()=>{
-    const user = new UserService.create(1, "marcocar", "Marco")
+    const user = UserService.create(1, "marcocar", "Marco")
 
     expect(user.username).toBe("marcocar")
     expect(user.name).toBe("Marco")
     expect(user.id).toBe(1)
     expect(user.bio).not.toBeUndefined()
   });
+})
