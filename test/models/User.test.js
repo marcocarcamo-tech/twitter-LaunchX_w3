8e7ebf027992ad048bf41a2f2af43af351b40435
Marco <macdev.gmail@gmail.com>
2022-04-29 22:56:33 -0500

    User class created and test passed correctly

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/user')
 
 describe("Unit test for User class", ()=>{
   test('Create an User object', ()=>{
-    const user = new User(1, "marcocar", "Marco", "Bio", "dataCreated", "lastUpdated")
+    const user = new User(1, "marcocar", "Marco", "Bio", "dateCreated", "lastUpdated")
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("marcocar")
     expect(user.name).toBe("Marco")
     expect(user.bio).toBe("Bio")
-    expect(user.dataCreated).toBe("dataCreated")
+    expect(user.dateCreated).toBe("dateCreated")
     expect(user.lastUpdated).toBe("lastUpdated")
   });
 })
